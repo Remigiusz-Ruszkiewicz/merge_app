7102525b516ea63dc2946358fc744c4af199e5b6
Remigiusz-Ruszkiewicz <RemikR2000@interia.pl>
2023-01-05 21:48:50 +0100

    merge someting

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'merge_app'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -48,68 +29,187 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _indexOfDroppedItem = 0;
+  bool _isDragging = false;
 
-  void _incrementCounter() {
+  void _acceptDraggedItem(int fromIndex,toIndex) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      try{
+        if(elements.firstWhere((element) => element.index==fromIndex).weight==elements.firstWhere((element) => element.index==toIndex).weight){
+          elements[toIndex] = Element(elements[toIndex].weight*2, Colors.lime, toIndex);
+          elements.removeAt(fromIndex);
+        }
+      }catch(_){
+        return;
+      }
+
+    });
+  }
+
+  void _setIsDragging() {
+    setState(() {
+      _isDragging = true;
+    });
+  }
+
+  void _resetIsDragging() {
+    setState(() {
+      _isDragging = false;
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  List<Element> elements = [Element(1, Colors.black, 0)];
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            GridView.count(
+              shrinkWrap: true,
+              primary: false,
+              crossAxisCount: 4,
+              children: List.generate(
+                16,
+                (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: elements.any((element) => element.index == index)
+                        ? Draggable<int>(
+                            data: index,
+                            childWhenDragging: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.blue,
+                                  ),
+                                  borderRadius:
+                                      const BorderRadius.all(Radius.circular(20))),
+                            ),
+                            onDragStarted: () {
+                              _setIsDragging();
+                            },
+                            onDraggableCanceled: (_, __) {
+                              _resetIsDragging();
+                            },
+                            onDragCompleted: () {
+                              print('SKONCZONE');
+                              _resetIsDragging();
+                            },
+                            feedback: Container(
+                              width: 100,
+                              height: 100,
+                              decoration: const BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20))),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: elements.firstWhere((element) => element.index == index).color,
+                                  border: Border.all(
+                                    color: Colors.red,
+                                  ),
+                                  borderRadius:
+                                      const BorderRadius.all(Radius.circular(10))),
+                              child: DragTarget<int>(
+                                builder: (
+                                    BuildContext context,
+                                    List<dynamic> accepted,
+                                    List<dynamic> rejected,
+                                    ) {
+                                  return Container(
+                                      );
+                                },
+                                onAccept: (int data) {
+                                  _acceptDraggedItem(data,index);
+                                },
+                              ),
+                            ),
+                          )
+                        : DragTarget<int>(
+                            builder: (
+                              BuildContext context,
+                              List<dynamic> accepted,
+                              List<dynamic> rejected,
+                            ) {
+                              return Container(
+                                  decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.green,
+                                ),
+                                borderRadius: BorderRadius.all(_isDragging
+                                    ? const Radius.circular(20)
+                                    : const Radius.circular(10)),
+                              ));
+                            },
+                            onAccept: (int data) {
+                              _acceptDraggedItem(data,index);
+                            },
+                          ),
+                  );
+                },
+              )
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: MaterialButton(
+                    onPressed: () => {
+                      setState(() {
+                        for(int i=0;i<16;i++){
+                          if(elements.any((element) => element.index == i)){
+                            continue;
+                          }
+                          else{
+                            elements.add(
+                              Element(2, Colors.green, i),
+                            );
+                            break;
+                          }
+                        }
+                      }),
+                    },
+                    color: Colors.red,
+                    child: Text('DODAJ'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: MaterialButton(
+                    onPressed: () => {
+                      setState(() {
+                        elements=[];
+                      }),
+                    },
+                    color: Colors.green,
+                    child: Text('CZYSC'),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Element {
+  Element(this.weight, this.color, this.index);
+
+  final int index;
+  final int weight;
+  final Color color;
 }
